c2d1d4366fabb3e892d8e586495c0d05febd6aea
Carlos Eduardo <educonts08@gmail.com>
2024-01-30 11:00:15 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,18 @@
+import { ptBR } from "date-fns/locale";
 import { Header } from "../components/Header";
-
+import { format } from 'date-fns'
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl">Olá, <strong>Eduardo!</strong></h2>
+        <p className="capitalize text-sm">{format(new Date(), "EEEE', 'dd 'de' MMMM", {
+          locale: ptBR
+        })}</p>
+      </div>
     </div>
   );
 }
